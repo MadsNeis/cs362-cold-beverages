51ad35b5098ccc2d4ce720e9ceb50a12bc77f459
MadsNeis <neismads@gmail.com>
2026-03-14 14:35:21 -0700

    Added test, item name and volume.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,4 +2,14 @@ require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
 
+  it 'has a name' do
+    item = Item.new('Milk', 10)
+    expect(item.name).to eq('Milk')
+  end
+
+  it 'has a volume' do
+    item = Item.new('Milk', 10)
+    expect(item.volume).to eq(10)
+  end
+
 end
