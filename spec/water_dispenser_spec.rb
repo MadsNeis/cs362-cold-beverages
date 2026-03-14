e438d8957dd377a33c1f98a25c309edf742ff778
MadsNeis <neismads@gmail.com>
2026-03-14 14:54:57 -0700

    Tests added, for WaterDispenser reservoir and behavior.

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,22 @@
 require_relative '../lib/water_dispenser'
+require_relative '../lib/water_reservoir'
+require_relative '../lib/vessel'
 
 describe 'A water dispenser' do
+
+  it 'has a reservoir' do
+    reservoir = WaterReservoir.new
+    dispenser = WaterDispenser.new(reservoir)
+    expect(dispenser.reservoir).to eq(reservoir)
+  end
+
+  it 'dispenses water from the reservoir based on vessel volume' do
+    reservoir = WaterReservoir.new(10, 10)
+    vessel = Vessel.new('Cup', 3)
+    dispenser = WaterDispenser.new(reservoir)
+    dispenser.dispense(vessel)
+    expect(reservoir.current_water_volume).to eq(7)
+  end
+
 
 end
